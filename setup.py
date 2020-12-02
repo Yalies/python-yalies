6183c5823e8918a62f32d1b15f4dc5a37de1dcbd
ErikBoesen <me@erikboesen.com>
2020-12-01 18:13:26 -0700

    Bump to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='yalies',
-      version='0.1.0',
+      version='0.2.0',
       description='Library for easy interaction with the Yalies API (yalies.io/apidocs)',
       url='https://github.com/Yalies/yalies-python',
       author='Erik Boesen',
