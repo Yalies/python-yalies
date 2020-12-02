41c004d21c78402338ee1e8cc4e4c8822d7b106e
ErikBoesen <me@erikboesen.com>
2020-12-01 18:13:07 -0700

    DRY example script logging

--- example.py
+++ example.py
@@ -1,13 +1,21 @@
 import yalies
 import os
 
+
+def log(students):
+    """
+    Utility function for logging lists of students in this script.
+    """
+    for student in students:
+        print(f'{student.last_name}, {student.first_name} - {student.college} {student.year}')
+
+
 # Initialize API with token from YALIES_TOKEN environment variable or console
 api = yalies.API(os.environ.get('YALIES_TOKEN') or input('Insert Yalies API token: '))
 
 # Perform a test request to the API
 johns = api.students(query='john')
-for student in johns:
-    print(f'{student.last_name}, {student.first_name} - {student.college} {student.year}')
+log(johns)
 
 
 import time
@@ -18,9 +26,7 @@ hopper_2024 = api.students(filters={
                                'year': [2024]
                            },
                            page=1)
-
-for student in hopper_2024:
-    print(f'{student.last_name}, {student.first_name} - {student.college} {student.year}')
+log(hopper_2024)
 
 
 # Get all students at once
@@ -30,5 +36,4 @@ print(f'There are {len(all_students)} undergraduate students at Yale.')
 
 # Get students by page with a custom page size
 third_five_students = api.students(page=3, page_size=5)
-for student in third_five_students:
-    print(f'{student.last_name}, {student.first_name} - {student.college} {student.year}')
+log(third_five_students)
