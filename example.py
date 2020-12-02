0d91da6eeafa21338261efc59dd455247ce30300
ErikBoesen <me@erikboesen.com>
2020-12-01 17:39:15 -0700

    Expand example

--- example.py
+++ example.py
@@ -4,5 +4,7 @@ import os
 # Initialize API with token from YALIES_TOKEN environment variable or console
 api = yalies.API(os.environ.get('YALIES_TOKEN') or input('Insert Yalies API token: '))
 
-# Perform a test request to the PAI
-print(api.students('erik'))
+# Perform a test request to the API
+eriks = api.students('erik')
+for erik in eriks:
+    print(f'{erik.name} - {erik.college} {erik.year}')
