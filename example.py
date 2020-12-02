258c4309945337ba1da7ae984451eba83ce54578
ErikBoesen <me@erikboesen.com>
2020-12-01 17:44:05 -0700

    Add more sophisticated example

--- example.py
+++ example.py
@@ -5,6 +5,19 @@ import os
 api = yalies.API(os.environ.get('YALIES_TOKEN') or input('Insert Yalies API token: '))
 
 # Perform a test request to the API
-eriks = api.students('erik')
-for erik in eriks:
-    print(f'{erik.name} - {erik.college} {erik.year}')
+johns = api.students(query='john')
+for student in johns:
+    print(f'{student.last_name}, {student.first_name} - {student.college} {student.year}')
+
+
+import time
+
+# Perform a more sophisticated request with filters and page
+hopper_2024 = api.students(filters={
+                               'college': ['Grace Hopper'],
+                               'year': [2024]
+                           },
+                           page=1)
+
+for student in hopper_2024:
+    print(f'{student.last_name}, {student.first_name} - {student.college} {student.year}')
