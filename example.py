74a678fedf19826a046c9143346a004c746e0cba
ErikBoesen <me@erikboesen.com>
2020-12-01 17:32:07 -0700

    Don't prompt for input in example unless no env var present

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import yalies
 import os
 
 # Initialize API with token from YALIES_TOKEN environment variable or console
-api = yalies.API(os.environ.get('YALIES_TOKEN', input('Insert Yalies API token: ')))
+api = yalies.API(os.environ.get('YALIES_TOKEN') or input('Insert Yalies API token: '))
 
 # Perform a test request to the PAI
 print(api.students('erik'))
